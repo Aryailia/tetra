b77e2fbd8b80afeb1016ecb214d52cdf9e8f9f44
Aryailia <Aryailia@users.noreply.github.com>
2022-08-22 21:30:02 +0400

    Fix navbar current styling

--- website/templates/website/navbar.sh
+++ website/templates/website/navbar.sh
@@ -7,10 +7,10 @@
 
 entry() {
   # $1: relpath of current file
-  # $2: relpath to test against
+  # $2: relstem with which to test against $1
   # $3: text to display in navbar
   # $4: url to link to
-  if [ "${1}" = "${2}" ]
+  if [ "${1%.*}" = "${2}" ]
     then printf '<span class="current">'
     else printf '<span>'
   fi
@@ -21,11 +21,10 @@ tag="${2:+"#${2}"}"  # Add the anchor if not blank
 
 outln() { printf %s\\n "$@"; }
 
-
 outln "${s}"'<nav id="top" class="link-hover-only-underline">'
-outln "${s}  $(    entry "${1}" "index.html"    "Home"     "" )<!--"
-outln "${s}  -->$( entry "${1}" "projects.html" "Projects" "projects.html" )<!--"
-outln "${s}  -->$( entry "${1}" "notes.html"    "Notes"    "notes.html" )<!--"
-outln "${s}  -->$( entry "${1}" "blog.html"     "Blog"     "blog${tag}" )<!--"
-outln "${s}  -->$( entry "${1}" "about.html"    "About"    "about.html" )<!--"
+outln "${s}  $(    entry "${1}" "index"    "Home"     "" )<!--"
+outln "${s}  -->$( entry "${1}" "projects" "Projects" "projects.html" )<!--"
+outln "${s}  -->$( entry "${1}" "notes"    "Notes"    "notes.html" )<!--"
+outln "${s}  -->$( entry "${1}" "blog"     "Blog"     "blog${tag}" )<!--"
+outln "${s}  -->$( entry "${1}" "about"    "About"    "about.html" )<!--"
 outln "${s}--></nav>"
